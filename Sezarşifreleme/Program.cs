dd3151298350e6d66ea3df1bcde993354caaf753
mehmetishaktas <95986929+mehmetishaktas@users.noreply.github.com>
2024-02-19 23:20:25 +0300

    Proje dosyası ekle.

---
+++ Sezarşifreleme/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int s1;
+string cevir;
+Random random = new Random();
+for(int j=1;j<=20; j++)
+{
+    for(int i = 1; i <= 1; i++)
+    {
+         s1 = random.Next(40,96);
+        cevir =Convert.ToString((char)s1);
+        Console.Write(cevir);
+       
+    }           
+}
